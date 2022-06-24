77db5b55259ad265e0985beb8294f4bf3d7ede31
Derek <nightwing917@hotmail.com>
2022-06-23 19:58:29 -0800

    added more lotto balls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logosvg';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 import Ball from './Ball';
 
@@ -6,7 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Ball num={17}/>
+        <Ball num={17} />
+        <Ball num={1} />
+        <Ball num={40} />
+        <Ball num={27} />
       </div>
     );
   }
