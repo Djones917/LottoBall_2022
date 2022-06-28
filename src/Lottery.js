f4571d4f62a81e5d2519868c215f1afae5a6f328
Derek <nightwing917@hotmail.com>
2022-06-27 19:26:32 -0800

    adding default props to lottery js

--- src/Lottery.js
+++ src/Lottery.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 
 class Lottery extends Component {
-
+    static defaultProps = {
+        title: 'Lotto',
+        maxBalls: 6,
+        maxNum: 40
+    }
 }
 
 export default Lottery;
